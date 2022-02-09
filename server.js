e0a2627fc95bdeac9e21a1ff67061fcc07214617
orgil0588 <76672549+orgil0588@users.noreply.github.com>
2022-02-09 19:59:27 +0800

    some fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bp = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 const connectDB = require("./config/db");
 const Sentiment = require("./models/Sentiment");
 const interval = require("./service/crawler");
@@ -10,13 +10,21 @@ connectDB();
 const port = process.env.PORT || 8080;
 
 app.use(bp.json());
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
-  interval();
-
-  res.status(200).json({
-    success: true,
-  });
+  interval()
+    .then((res) => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.status(200).json *
+        {
+          success: false,
+          error: err,
+        };
+    });
 });
 
 app.get("/data", async (req, res) => {
